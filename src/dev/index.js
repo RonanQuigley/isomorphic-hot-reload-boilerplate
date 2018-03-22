ba61ab033483b2b73beaca9eb90c9982b154dc27
ronanquigley <info@ronanquigley.com>
2018-03-22 09:45:44 +0000

    additional commenting and refactor of dev

--- src/dev/index.js
+++ src/dev/index.js
@@ -1,17 +1,19 @@
 import chokidar from 'chokidar';
 import express from 'express';
 import webpack from 'webpack';
-import clientConfig from '../../webpack/front-end/webpack.dev.babel';
-import serverConfig from '../../webpack/back-end/webpack.dev.babel';
-import hotMiddleware from 'webpack-hot-middleware';
-import devMiddleware from 'webpack-dev-middleware';
-import hotServerMiddleware from 'webpack-hot-server-middleware';
 import open from 'opn';
 import path from 'path';
+import devMiddleware from 'webpack-dev-middleware';
+import hotClientMiddleware from 'webpack-hot-middleware';
+import hotServerMiddleware from 'webpack-hot-server-middleware';
+import clientConfig from '../../webpack/front-end/webpack.dev.babel';
+import serverConfig from '../../webpack/back-end/webpack.dev.babel';
 
 const router = express.Router();
 const clientCompiler = webpack(clientConfig);
 const mergedCompilers = webpack([clientConfig, serverConfig]);
+const serverDir = path.resolve(__dirname, '../server'),
+const watcher = chokidar.watch(serverDir);
 
 const builtDevServer = devMiddleware(mergedCompilers, {
     serverSideRender: true, stats: 'none'
@@ -21,19 +23,9 @@ const builtDevClient = devMiddleware(clientCompiler, {
     noInfo: true, publicPath: clientConfig.output.publicPath, stats: 'none'
 });
 
-const serverMiddleware = hotServerMiddleware(mergedCompilers);
+const builtHotServer = hotServerMiddleware(mergedCompilers);
 
-const builtHotClient = hotMiddleware(clientCompiler);
-
-builtDevServer.waitUntilValid(() => {
-    open("http://localhost:" + (process.env.PORT || 3000), {
-        // app: ['chrome', '--incognito'] 
-    });
-});
-
-const watcher = chokidar.watch(
-    path.resolve(__dirname, '../server'),
-);
+const builtHotClient = hotClientMiddleware(clientCompiler);
 
 // watch our server side files for changes
 watcher.on('ready', () => {
@@ -43,12 +35,19 @@ watcher.on('ready', () => {
     })
 });
 
+builtDevServer.waitUntilValid(() => {
+    open("http://localhost:" + (process.env.PORT || 3000), {
+        // app: ['chrome', '--incognito'] 
+    });
+});
+
 router.use(builtDevServer);
 
+// built client middleware must come before the hot server
 router.use(builtDevClient);
 
 router.use(builtHotClient)
 
-router.use(serverMiddleware);
+router.use(builtHotServer);
 
 export default router;
