f5c036199b724404f6b72f6f7a664befc367b8a1
ronanquigley <info@ronanquigley.com>
2018-03-23 16:46:15 +0000

    removed debug from lint

--- src/client/index.js
+++ src/client/index.js
@@ -4,8 +4,8 @@ import App from './app';
 const root = document.getElementById('root');
 
 // use render in development, hydrate in production
-const renderMethod = !!module.hot ? ReactDOM.render : ReactDOM.hydrate 
-renderMethod(<App />, root)
+const renderMethod = !!module.hot ? ReactDOM.render : ReactDOM.hydrate; 
+renderMethod(<App />, root);
 
 // change me to a different colour and see the changes reflected in browser
 document.body.style.background = 'white';
