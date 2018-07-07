5b1ae7a1cc738d2bbaa67317d69cb645e07be30e
ronanquigley <info@ronanquigley.com>
2018-07-07 18:33:53 +0100

    removal of test queries. app is optional for ssr

--- src/client/api/graphql/index.js
+++ src/client/api/graphql/index.js
@@ -2,65 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import testQuery from './query.graphql';
-import testMutation from './mutation.graphql';
 import { Query, ApolloProvider, Mutation } from 'react-apollo';
 
 const client = new ApolloClient({
     uri: 'http://localhost:3000/graphql'
 });
 
-const TestMutation = () => {
-    let input = React.createRef();
-
-    function handleClick() {
-        input.current.focus();
-    }
-
-    function getValues(data) {
-        return Object.keys(data.signup).map(item => (
-            <div key={item}>{data.signup[item]}</div>
-        ));
-    }
-
-    return (
-        <Mutation mutation={testMutation}>
-            {(signup, { loading, error, data }) => {
-                console.log(data);
-                return (
-                    <div>
-                        <form
-                            onSubmit={e => {
-                                e.preventDefault();
-                                signup();
-                            }}
-                        >
-                            <input ref={input} />
-                            <button type="submit">Add Todo</button>
-                        </form>
-                        {loading && <div>Loading</div>}
-                        {error && <div>error</div>}
-                        {data && <div>{getValues(data)}</div>}
-                    </div>
-                );
-            }}
-        </Mutation>
-    );
-};
-
-const TestQuery = () => (
-    <Query query={testQuery}>
-        {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-            return data.users.map(user => <div key={user.id}>{user.id}</div>);
-        }}
-    </Query>
-);
-
 const Apollo = () => (
     <ApolloProvider client={client}>
-        {/* <TestQuery /> */}
-        <TestMutation />
+        <div>Add your queries to apollo provider using the query component</div>
     </ApolloProvider>
 );
 
--- src/common/react/components/html/index.js
+++ src/common/react/components/html/index.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 export default class Html extends PureComponent {
     static propTypes = {
-        app: PropTypes.string.isRequired,
+        // optional in case you don't want to
+        // render a react app and just need html
+        app: PropTypes.string,
         styles: PropTypes.string.isRequired,
         title: PropTypes.string,
         scripts: PropTypes.array
